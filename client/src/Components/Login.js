6f3c92be275567daccece6eb42ccd27e6a92224d
Web Development II <M.Develop2@utas.edu.om>
2025-10-06 12:19:04 +0400

    act2

--- client/src/Components/Login.js
+++ client/src/Components/Login.js
@@ -1,11 +1,68 @@
 import loginimage from "../Images/loginImage.jpg";
 import "../App.css";
+import {
+  Container,
+  Row,
+  Col,
+  Form,
+  FormGroup,
+  Label,
+  Input,
+  Button,
+} from "reactstrap";
+import { Link } from "react-router-dom";
 
 const Login = () => {
   return (
     <div>
       <h1>Login</h1>
       <img src={loginimage} className="loginsmall" />
+
+      <Container>
+        <Form>
+          <Row>
+            <Col md={3}>
+              <FormGroup>
+                <Label for="exampleEmail">Email</Label>
+                <Input
+                  id="exampleEmail"
+                  name="email"
+                  placeholder="with a placeholder"
+                  type="email"
+                />
+              </FormGroup>
+            </Col>
+          </Row>
+
+          <Row>
+            <Col md={3}>
+              <FormGroup>
+                <Label for="examplePassword">Password</Label>
+                <Input
+                  id="examplePassword"
+                  name="password"
+                  placeholder="password placeholder"
+                  type="password"
+                />
+              </FormGroup>
+            </Col>
+          </Row>
+
+          <Row>
+            <Col md={3}>
+              <Button>login</Button>
+            </Col>
+          </Row>
+
+          <Row>
+            <Col md={3}></Col>
+          </Row>
+        </Form>
+      </Container>
+
+      <p className="smalltext">
+        No Account? <Link to="/register">Sign Up now.</Link>
+      </p>
     </div>
   );
 };
--- client/src/Components/Register.js
+++ client/src/Components/Register.js
@@ -1,7 +1,93 @@
+import { userSchemaValidation } from "../Validations/UserValidations";
+import * as yup from "yup";
+import { useForm } from "react-hook-form";
 
+import { yupResolver } from "@hookform/resolvers/yup";
+import { Container, Row, Col, Button } from "reactstrap";
 const Register = () => {
+  const userList = [
+    { id: 1, name: "Test User", email: "test@example.com", password: "123456" },
+  ];
+  //For form validation using react-hook-form
+
+  const {
+    register,
+
+    handleSubmit, // Submit the form when this is called
+
+    formState: { errors },
+  } = useForm({
+    resolver: yupResolver(userSchemaValidation), //Associate your Yup validation schema using the resolver
+  });
+
+  // Handle form submission
+
+  const onSubmit = (data) => {
+    console.log("Form Data", data); // You can handle the form submission here
+    alert("Validation all good.");
+  };
   return (
-    <h1>Register</h1>
+    <Container>
+      <form className="div-form" onSubmit={handleSubmit(onSubmit)}>
+        <Row>
+          <Col md={6}>
+            Name<br></br>
+            <input type="text" name="name" {...register("name")}></input>
+          </Col>
+          <p className="error">{errors.name?.message}</p>
+        </Row>
+        <Row>
+          <Col md={6}>
+            Email<br></br>
+            <input type="email" name="email" {...register("email")}></input>
+          </Col>
+          <p className="error">{errors.email?.message}</p>
+        </Row>
+        <Row>
+          <Col md={6}>
+            Password<br></br>
+            <input
+              type="password"
+              name="password"
+              {...register("password")}
+            ></input>
+          </Col>
+          <p className="error">{errors.password?.message}</p>
+        </Row>
+        <Row>
+          <Col md={6}>
+            Confirm Password<br></br>
+            <input
+              type="password"
+              name="confirmpassword"
+              {...register("confirmPassword")}
+            ></input>
+          </Col>
+          <p className="error">{errors.confirmPassword?.message}</p>
+        </Row>
+        <Row>
+          <Col md={6}>
+            <Button>Register</Button>
+          </Col>
+        </Row>
+      </form>
+      <Row>
+        <Col md={6}>
+          <h1>List of Users</h1>
+          <table className="table">
+            <tbody>
+              {userList.map((user) => (
+                <tr key={user.id}>
+                  <td>{user.name}</td>
+                  <td>{user.email}</td>
+                  <td>{user.password}</td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </Col>
+      </Row>
+    </Container>
   );
 };
 
