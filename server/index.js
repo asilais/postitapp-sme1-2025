3178106b4c7284c4a38824a006c2a7fc517e5c40
Web Development II <M.Develop2@utas.edu.om>
2025-10-30 09:51:00 +0400

    act5

---
+++ server/index.js
@@ -0,0 +1,43 @@
+import mongoose from "mongoose";
+import cors from "cors";
+import express from "express";
+import UserModel from "./Models/UserModel.js";
+import bcrypt from "bcrypt";
+
+const app = express();
+app.use(express.json());
+app.use(cors());
+
+//Database connection
+const connectString =
+  "mongodb+srv://2020393031_db_user:As18930298@postitcluster.iqiywsu.mongodb.net/postITDb?appName=PostITCluster";
+mongoose.connect(connectString, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+//API for register user
+
+app.post("/registerUser", async (req, res) => {
+  try {
+    const name = req.body.name;
+    const email = req.body.email;
+    const password = req.body.password;
+    const hashedpassword = await bcrypt.hash(password, 10);
+
+    const user = new UserModel({
+      name: name,
+      email: email,
+      password: hashedpassword,
+    });
+    await user.save();
+    res.send({ user: user, msg: "Added." });
+  } catch (error) {
+    //res.status(500).json({ error: "An error occurred" });
+    console.log(error);
+  }
+});
+
+app.listen(3001, () => {
+  console.log("You are connected");
+});
